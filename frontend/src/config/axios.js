74bf696e3cb89b2f5a109e6c8450355877750566
gundar16 <119607641+gundar16@users.noreply.github.com>
2025-04-30 15:52:00 +0100

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_URL,
+  baseURL: import.meta.env.VITE_REACT_APP_URL || 'http://localhost:3002',
 });
 
 export default instance;
